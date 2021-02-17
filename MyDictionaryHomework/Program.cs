fbc34e306426f918fc29268b0ff8903590f94618
BusOzturk <busraozturk66@gmail.com>
2021-02-17 17:46:09 +0300

    Add project files.

---
+++ MyDictionaryHomework/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionaryHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> Liste = new MyDictionary<string, int>();
+            Liste.Add("Büşra", 25);
+            Liste.Add("Cahit", 30);
+            Liste.Add("Bekir", 62);
+            Liste.Add("Pınar", 27);
+            Liste.Add("Halime", 56);
+
+            Liste.List();
+
+            
+           
+            
+
+        }
+    }
+}
